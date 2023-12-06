1be4c6eb9fa899935e4ff94d9b6c66449eb626c5
Eric Hamiter <ehamiter@gmail.com>
2023-12-06 08:40:28 -0600

    Refactor promote.py to archive only optionally; add in carousel image navigation

--- promote.py
+++ promote.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import sys
 import boto3
@@ -35,67 +36,76 @@ ch.setFormatter(CustomFormatter())
 logger.addHandler(ch)
 
 
-if len(sys.argv) < 2:
-    print('Example usage: python promote.py landscape-2023-12-03T01/50/17.205070Z')
-    sys.exit(1)
+def main(file_tag, replace=False):
+    if len(sys.argv) < 2:
+        print('Example usage: python promote.py landscape-2023-12-03T01/50/17.205070Z --replace')
+        sys.exit(1)
 
 
-def extract_datetime(filename):
-    # Extract the part after "landscape-" or "portrait-" and keep 'Z' at the end
-    datetime_part = filename.split('-', 1)[1]
-    # Replace slashes with colons in the time part
-    datetime_with_colons = datetime_part.replace('/', ':')
-    return datetime_with_colons
+    def extract_datetime(filename):
+        # Extract the part after "landscape-" or "portrait-" and keep 'Z' at the end
+        datetime_part = filename.split('-', 1)[1]
+        # Replace slashes with colons in the time part
+        datetime_with_colons = datetime_part.replace('/', ':')
+        return datetime_with_colons
 
-def upload_file_to_s3(local_path, bucket, s3_key):
-    logger.info(f"Uploading file {local_path}...")
+    def upload_file_to_s3(local_path, bucket, s3_key):
+        logger.info(f"Uploading file {local_path}...")
 
-    try:
-        s3 = boto3.client(
-            "s3",
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        )
+        try:
+            s3 = boto3.client(
+                "s3",
+                aws_access_key_id=AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            )
 
-        # Set content-type so we're not downloading objects
-        if local_path.endswith('.txt'):
-            extra_args = {'ContentType': 'text/plain'}
-        elif local_path.endswith('.webp'):
-            extra_args = {'ContentType': 'image/webp'}
-        else:
-            extra_args = {}
+            # Set content-type so we're not downloading objects
+            if local_path.endswith('.txt'):
+                extra_args = {'ContentType': 'text/plain'}
+            elif local_path.endswith('.webp'):
+                extra_args = {'ContentType': 'image/webp'}
+            else:
+                extra_args = {}
 
-        # Upload the file with specified content type
-        s3.upload_file(local_path, bucket, s3_key, ExtraArgs=extra_args)
+            # Upload the file with specified content type
+            s3.upload_file(local_path, bucket, s3_key, ExtraArgs=extra_args)
 
-        logger.info(f"File {local_path} uploaded to {bucket}/{s3_key}")
+            logger.info(f"File {local_path} uploaded to {bucket}/{s3_key}")
 
-    except FileNotFoundError:
-        logger.error("The file was not found")
-    except NoCredentialsError:
-        logger.error("Credentials not available")
+        except FileNotFoundError:
+            logger.error("The file was not found")
+        except NoCredentialsError:
+            logger.error("Credentials not available")
 
-input_filename = sys.argv[1]
-the_datetime = extract_datetime(input_filename)
-the_date = the_datetime.split('T')[0]
+    input_filename = sys.argv[1]
+    the_datetime = extract_datetime(input_filename)
+    the_date = the_datetime.split('T')[0]
 
-# Local file paths using the full datetime so you can generate more than one per day
-landscape_file = f"./staging/landscape-{the_datetime}.webp"
-portrait_file = f"./staging/portrait-{the_datetime}.webp"
-prompt_original_file = f"./staging/original-{the_datetime}.txt"
-prompt_landscape_file = f"./staging/landscape-{the_datetime}.txt"
-prompt_portrait_file = f"./staging/portrait-{the_datetime}.txt"
+    # Local file paths using the full datetime so you can generate more than one per day
+    landscape_file = f"./staging/landscape-{the_datetime}.webp"
+    portrait_file = f"./staging/portrait-{the_datetime}.webp"
+    prompt_original_file = f"./staging/original-{the_datetime}.txt"
+    prompt_landscape_file = f"./staging/landscape-{the_datetime}.txt"
+    prompt_portrait_file = f"./staging/portrait-{the_datetime}.txt"
 
-# For immediate usage
-upload_file_to_s3(landscape_file, AWS_S3_BUCKET, f"images/landscape.webp")
-upload_file_to_s3(portrait_file, AWS_S3_BUCKET, f"images/portrait.webp")
-upload_file_to_s3(prompt_original_file, AWS_S3_BUCKET, f"prompts/original.txt")
-upload_file_to_s3(prompt_landscape_file, AWS_S3_BUCKET, f"prompts/landscape.txt")
-upload_file_to_s3(prompt_portrait_file, AWS_S3_BUCKET, f"prompts/portrait.txt")
+    if replace:
+        # For immediate usage
+        upload_file_to_s3(landscape_file, AWS_S3_BUCKET, f"images/landscape.webp")
+        upload_file_to_s3(portrait_file, AWS_S3_BUCKET, f"images/portrait.webp")
+        upload_file_to_s3(prompt_original_file, AWS_S3_BUCKET, f"prompts/original.txt")
+        upload_file_to_s3(prompt_landscape_file, AWS_S3_BUCKET, f"prompts/landscape.txt")
+        upload_file_to_s3(prompt_portrait_file, AWS_S3_BUCKET, f"prompts/portrait.txt")
 
-# Archival
-upload_file_to_s3(landscape_file, AWS_S3_BUCKET, f"images/{the_date}-landscape.webp")
-upload_file_to_s3(portrait_file, AWS_S3_BUCKET, f"images/{the_date}-portrait.webp")
-upload_file_to_s3(prompt_original_file, AWS_S3_BUCKET, f"prompts/{the_date}-original.txt")
-upload_file_to_s3(prompt_landscape_file, AWS_S3_BUCKET, f"prompts/{the_date}-landscape.txt")
-upload_file_to_s3(prompt_portrait_file, AWS_S3_BUCKET, f"prompts/{the_date}-portrait.txt")
+    # Archival
+    upload_file_to_s3(landscape_file, AWS_S3_BUCKET, f"images/{the_date}-landscape.webp")
+    upload_file_to_s3(portrait_file, AWS_S3_BUCKET, f"images/{the_date}-portrait.webp")
+    upload_file_to_s3(prompt_original_file, AWS_S3_BUCKET, f"prompts/{the_date}-original.txt")
+    upload_file_to_s3(prompt_landscape_file, AWS_S3_BUCKET, f"prompts/{the_date}-landscape.txt")
+    upload_file_to_s3(prompt_portrait_file, AWS_S3_BUCKET, f"prompts/{the_date}-portrait.txt")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload files to S3 with optional replacement.")
+    parser.add_argument("file_tag", type=str, help="Tag of the file to upload")
+    parser.add_argument("--replace", action="store_true", help="Replace existing files if set to True")
+    args = parser.parse_args()
+    main(args.file_tag, args.replace)
